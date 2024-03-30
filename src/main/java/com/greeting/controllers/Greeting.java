7498e1827efa9756fbdd795a3a8bff6f3150a2a1
Daniel de Jesus <danieldejesus8745@gmail.com>
2024-03-30 18:34:25 -0300

    Getting hostname

--- src/main/java/com/greeting/controllers/Greeting.java
+++ src/main/java/com/greeting/controllers/Greeting.java
@@ -4,13 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping(path = "api/v1/greeting")
 public class Greeting {
 
     @GetMapping
-    public String greeting() {
-        return "Hello World!";
+    public String greeting() throws UnknownHostException {
+        try {
+            return "Hello World from: " + InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException exception) {
+            return "Error when trying get HOSTNAME";
+        }
     }
 
 }
